a89e339371dff5aa0da45d60a6cf16fe84526232
dslisleedh <gnova6kud@gmail.com>
2022-12-30 12:42:41 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# NCNet-flax [[Paper]](https://arxiv.org/abs/2208.11609)
+
+Unofficial implementation of NCNet using flax and jax.
+
+TODO:
+ - [X] Implement NCNet
+ - [ ] Implement training code(Using Div2K dataset in tfds)
+ - [ ] Implement tensorflow lite conversion code
---
+++ src/model.py
@@ -0,0 +1,60 @@
+import jax
+import jax.numpy as jnp
+from jax import lax
+import einops
+
+import flax
+from flax import linen as nn
+
+from typing import Optional
+
+
+def nearest_conv_init(
+        input_c: int, out_c: int, scale: int
+) -> jnp.ndarray:
+    kernel = jnp.transpose(
+        jnp.reshape(
+            jnp.repeat(
+                jnp.eye(input_c), (out_c * (scale ** 2)) // input_c, axis=0
+            ), (1, 1, -1, input_c)
+        ), (0, 1, 3, 2)
+    )
+    return kernel
+
+
+class NCNet(nn.Module):
+    n_filters: int
+    scale_factor: int
+    out_c: Optional[int] = None
+
+    @nn.compact
+    def __call__(self, x):
+        out_c = self.out_c or x.shape[-1]
+        nearest_conv_kernel = self.variable(
+            'nearest_conv', 'kernel', lambda: nearest_conv_init(x.shape[-1], out_c, self.scale_factor)
+        )
+
+        skip = lax.conv_general_dilated(
+            x, nearest_conv_kernel.value, (1, 1), 'VALID', dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
+        )
+        for i in range(7):
+            x = nn.Conv(
+                self.n_filters if i < 5 else out_c * (self.scale_factor ** 2), (3, 3),
+                padding='SAME', kernel_init=nn.initializers.glorot_normal()
+            )(x)
+            if i != 6:
+                x = nn.relu(x)
+        x += skip
+        x = einops.rearrange(
+            x, 'b h w (s1 s2 c) -> b (h s1) (w s2) c', s1=self.scale_factor, s2=self.scale_factor
+        )
+        x = jnp.clip(x, 0., 255.)
+        return x
+
+
+if __name__ == '__main__':
+    # Test and Visualize
+    x = jnp.ones((1, 256, 256, 3))
+    model = NCNet(64, 3)
+    rng = jax.random.PRNGKey(0)
+    print(model.tabulate(rng, x))
